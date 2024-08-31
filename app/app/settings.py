642c21ae892d1eeac2be844fe700a4b27bcd0f24
Øivind Skjervheim <oskjervheim@gmail.com>
2024-08-31 20:01:24 +0200

    added static root

--- app/app/settings.py
+++ app/app/settings.py
@@ -134,5 +134,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'polls/static/'
+]
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
