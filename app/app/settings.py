ff103519ab9bdde8e1e99abb656274546c51ddfc
Øivind Skjervheim <oskjervheim@gmail.com>
2025-03-15 20:58:47 +0100

    collect debug from .env

--- app/app/settings.py
+++ app/app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('BRANNJR_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 ALLOWED_HOSTS = os.environ.get('SERVERNAMES').split(' ')
 
 
